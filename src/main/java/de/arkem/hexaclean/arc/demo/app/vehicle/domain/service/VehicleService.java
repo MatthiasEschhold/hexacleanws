078e7fa6c31ef27e3d8501747f9910325ecc2da3
Matthias Eschhold <84323232+MatthiasEschhold@users.noreply.github.com>
2023-06-28 10:56:00 +0200

    renamings and improvements

--- src/main/java/de/arkem/hexaclean/arc/demo/app/vehicle/domain/service/VehicleService.java
+++ src/main/java/de/arkem/hexaclean/arc/demo/app/vehicle/domain/service/VehicleService.java
@@ -1,31 +1,28 @@
 package de.arkem.hexaclean.arc.demo.app.vehicle.domain.service;
 
 import de.arkem.hexaclean.arc.demo.app.vehicle.domain.model.Vehicle;
-import de.arkem.hexaclean.arc.demo.app.vehicle.domain.model.VehicleMasterData;
 import de.arkem.hexaclean.arc.demo.app.vehicle.domain.model.Vin;
 import de.arkem.hexaclean.arc.demo.app.vehicle.usecase.in.VehicleQuery;
-import de.arkem.hexaclean.arc.demo.app.vehicle.usecase.out.FetchVehicleMasterData;
 import de.arkem.hexaclean.arc.demo.app.vehicle.usecase.out.VehicleDbQuery;
 
 public class VehicleService implements VehicleQuery {
 
     private VehicleDbQuery vehicleDbQuery;
-    private FetchVehicleMasterData fetchVehicleMasterData;
 
-    public VehicleService(VehicleDbQuery vehicleDbQuery, FetchVehicleMasterData fetchVehicleMasterData) {
+    public VehicleService(VehicleDbQuery vehicleDbQuery) {
         this.vehicleDbQuery = vehicleDbQuery;
-        this.fetchVehicleMasterData = fetchVehicleMasterData;
     }
 
     public Vehicle findByVin(Vin vin) {
         Vehicle vehicle = vehicleDbQuery.findVehicleByVin(vin);
-        enrichWithVehicleMasterData(vehicle);
+        //add enrichment of vehcile master data here
         return vehicle;
     }
 
+    /*
     private void enrichWithVehicleMasterData(Vehicle vehicle) {
         VehicleMasterData masterData = fetchVehicleMasterData.fetch(vehicle.getVin());
         vehicle.addVehicleMasterData(masterData);
-    }
+    }*/
 
 }
